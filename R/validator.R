2c729053c62a94bb2fb698abe40a54a068202b3b
jday7879 <57715292+Jday7879@users.noreply.github.com>
2026-02-18 15:35:23 +0000

    remove checking types

--- R/validator.R
+++ R/validator.R
@@ -101,14 +101,6 @@ assign_agent_typechecks <- function(agent,schema){
     }
   }
 
-  agent <- agent |>
-    pointblank::col_is_integer(columns = eval(int_columns)) |>
-    pointblank::col_is_date(columns = eval(date_columns)) |>
-    pointblank::col_is_logical(columns = eval(logical_columns)) |>
-    pointblank::col_is_numeric(columns = eval(numeric_columns)) |>
-    pointblank::col_is_factor(columns = eval(factor_columns)) |>
-    pointblank::col_is_character(columns = eval(char_columns))
-
   return(agent)
 }
 
